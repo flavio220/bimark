df5c7979861dadce031ce1bb4f44fd7aeaed33a9
ADANTCHEDE <136079217+flavio220@users.noreply.github.com>
2026-06-11 09:02:21 +0100

    Delete src directory

--- src/app/buyer-dashboard/settings/page.jsx
+++
@@ -1,9 +0,0 @@
-import Header from '@/components/common/Header';
-import DashboardSidebar from '@/components/common/DashboardSidebar';
-import BuyerAccountSettings from './components/BuyerAccountSettings';
-
-export const metadata = { title: 'Paramètres du compte - Bimark' };
-
-export default function BuyerSettingsPage() {
-  return (<><Header /><DashboardSidebar userRole="buyer" /><BuyerAccountSettings /></>);
-}
--- src/app/buyer-dashboard/suppliers/page.jsx
+++
@@ -1,9 +0,0 @@
-import Header from '@/components/common/Header';
-import DashboardSidebar from '@/components/common/DashboardSidebar';
-import BuyerSuppliersManager from './components/BuyerSuppliersManager';
-
-export const metadata = { title: 'Mes Fournisseurs - Bimark' };
-
-export default function BuyerSuppliersPage() {
-  return (<><Header /><DashboardSidebar userRole="buyer" /><BuyerSuppliersManager /></>);
-}
